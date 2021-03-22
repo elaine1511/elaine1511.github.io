7029de39cf0f4adaf884df1abfdba1bc28ca5f37
elaine1511 <elainetmo1511@gmail.com>
2021-03-22 17:31:31 -0500

    finish lab

--- CS303/labW1D1/app.js
+++ CS303/labW1D1/app.js
@@ -3,20 +3,19 @@
 "use strict";
 
 function pow(x, n) {
-	return x * n;
+	return x ** n;
 }
 
 function add2(n1, n2) {
 	return n1 + n2;
 }
 
-function add(...args) {
+function add(arguments) {
 	let count = arguments.length;
 	let sum = 0;
-	for (let i=0; i<count; i++) {
-		sum += arguments[i];		
+	for (let i = 0; i < count; i++) {
+		sum += arguments[i];
 	}
-	
 	return sum;
 }
 
@@ -30,6 +29,18 @@ function add(...args) {
 	0 - 59   NC
 */
 function computeGrade(grades) {
-	return "???";
+	for (let score in grades) {
+		if (score >= 90) {
+			return "A";
+		} else if (score >= 80) {
+			return "B";
+		} else if (score >= 70) {
+			return "C";
+		} else if (score >= 60) {
+			return "D";
+		} else {
+			return "NC";
+		}
+	}
 }
 
